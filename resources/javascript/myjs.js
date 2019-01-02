e408ea158576cbc95e988ccc1e1b6b1cd12d189e
stern15 <ssemasuka@gmail.com>
2019-01-02 15:17:28 +0200

    scroll effect added

--- resources/javascript/myjs.js
+++ resources/javascript/myjs.js
@@ -1,25 +1,61 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(document).ready(function () {
+  // sticky navigation
+  $('.js--section-mission').waypoint(function (direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: "130px;"
+  });
+  // scroll on buttons "hire us"
+  $('.js--scroll-to-contact').click(function () {
+    $("html,body").animate({
+      scrollTop: $(".js--section-plans").offset().top - 100
+    }, 1000);
+  });
+  // scroll on buttons "Our work"
+  $('.js--scroll-to-work').click(function () {
+    $("html,body").animate({
+      scrollTop: $(".js--section-work").offset().top - 100
+    }, 1000);
+  });
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  })
+  // navigation scrool
+  $(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - 120
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  // animation on scroll
+  $('.js--wp-1').waypoint(function (direction) {
+    $('.js--wp-1').addClass('animated fadeInUp')
+  }, {
+    offset: '50%'
+  });
+  $('.js--wp-2').waypoint(function (direction) {
+    $('.js--wp-2').addClass('animated fadeInLeft')
+  }, {
+    offset: '50%'
+  });
+  $('.js--wp-3').waypoint(function (direction) {
+    $('.js--wp-3').addClass('animated fadeInRight')
+  }, {
+    offset: '50%'
+  });
+  $('.js--wp-4').waypoint(function (direction) {
+    $('.js--wp-4').addClass('animated fadeIn')
+  }, {
+    offset: '50%'
+  });
 });
-
